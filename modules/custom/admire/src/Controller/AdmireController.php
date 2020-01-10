51b18171fb16185cfafeb7ec1f478ba528f0778e
Alexander Bidenko <alexbidenko1998@gmail.com>
2020-01-10 16:47:35 +0300

    Test

--- modules/custom/admire/src/Controller/AdmireController.php
+++ modules/custom/admire/src/Controller/AdmireController.php
@@ -12,33 +12,33 @@ class AdmireController extends ControllerBase {
                 '#type' => 'markup',
                 '#markup' =>
 '
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-4">
-            <div class="btn-group btn-group-toggle">
-              <label class="btn btn-secondary active">
-                <input type="radio" name="options" value="id" id="option1" v-model="sorting"> Id
-              </label>
-              <label class="btn btn-secondary">
-                <input type="radio" name="options" value="name" id="option2" v-model="sorting"> Name
-              </label>
-              <label class="btn btn-secondary">
-                <input type="radio" name="options" value="description" id="option3" v-model="sorting"> Description
-              </label>
+<div id="App">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-4">
+                <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                  <label class="btn btn-secondary active">
+                    <input type="radio" name="options" value="id" id="option1" v-model="sorting"> Id
+                  </label>
+                  <label class="btn btn-secondary">
+                    <input type="radio" name="options" value="name" id="option2" v-model="sorting"> Name
+                  </label>
+                  <label class="btn btn-secondary">
+                    <input type="radio" name="options" value="description" id="option3" v-model="sorting"> Description
+                  </label>
+                </div>
             </div>
-        </div>
-        <div class="col-8">
-            <div class="form-group row">
-                <label for="inputEmail3" class="col-sm-2 col-form-label" v-model="filterName">Name</label>
-                <div class="col-sm-10">
-                  <input type="email" class="form-control" id="inputEmail3">
+            <div class="col-8">
+                <div class="form-group row">
+                    <label for="inputEmail3" class="col-sm-2 col-form-label" v-model="filterName">Name</label>
+                    <div class="col-sm-10">
+                      <input type="email" class="form-control" id="inputEmail3">
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<div id="App">
     <table class="table">
       <thead>
         <tr>
