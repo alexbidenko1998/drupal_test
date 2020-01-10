86f30ff382b3aaf15b3ae7fcd3097137b9ef5a42
Alexander Bidenko <alexbidenko1998@gmail.com>
2020-01-10 17:25:39 +0300

    Test

--- modules/custom/admire/src/Controller/AdmireController.php
+++ modules/custom/admire/src/Controller/AdmireController.php
@@ -6,59 +6,59 @@ use Drupal\Core\Controller\ControllerBase;
 class AdmireController extends ControllerBase {
 
     public function content() {
+        $form['search'] = array(
+            '#type' => 'text',
+            '#title' => 'Имя',
+            '#v-model' => 'filterName'
+        );
         return [
             'title' => 'Список мероприятий',
             'raw_markup' => [
-                '#type' => 'markup',
-                '#markup' =>
+                '#prefix' => '<div id="App">',
+                '#sufix' => '</div>',
+                'search' => $form,
+                'table' => [
+                    '#type' => 'markup',
+                    '#markup' =>
 '
-<div id="App">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-4">
-                <div class="btn-group btn-group-toggle" data-toggle="buttons">
-                  <label class="btn btn-secondary active">
-                    <img :is="input" type="radio" name="options" value="id" id="option1" v-model="sorting"> Id
-                  </label>
-                  <label class="btn btn-secondary">
-                    <img :is="input" type="radio" name="options" value="name" id="option2" v-model="sorting"> Name
-                  </label>
-                  <label class="btn btn-secondary">
-                    <img :is="input" type="radio" name="options" value="description" id="option3" v-model="sorting"> Description
-                  </label>
-                </div>
-            </div>
-            <div class="col-8">
-                <div class="form-group row">
-                    <p :is="label" for="inputEmail3" class="col-sm-2 col-form-label" v-model="filterName">Name</p>
-                    <div class="col-sm-10">
-                      <img :is="input" type="email" class="form-control" id="inputEmail3">
-                    </div>
-                </div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-4">
+            <div class="btn-group btn-group-toggle" data-toggle="buttons">
+              <p :is="label" class="btn btn-secondary active">
+                <img :is="input" type="radio" name="options" value="id" id="option1" v-model="sorting"> Id
+              </p>
+              <p :is="label" class="btn btn-secondary">
+                <img :is="input" type="radio" name="options" value="name" id="option2" v-model="sorting"> Name
+              </p>
+              <p :is="label" class="btn btn-secondary">
+                <img :is="input" type="radio" name="options" value="description" id="option3" v-model="sorting"> Description
+              </p>
             </div>
         </div>
     </div>
-
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">Id</th>
-          <th scope="col">Название</th>
-          <th scope="col">Описание</th>
-          <th scope="col">Ссылка</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="event in filteredEvents">
-          <th scope="row">{{event.id}}</th>
-          <td>{{event.name}}</td>
-          <td>{{event.description}}</td>
-          <td><a :href="event.site">{{event.site}}</td>
-        </tr>
-      </tbody>
-    </table>
 </div>
-',
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">Название</th>
+      <th scope="col">Описание</th>
+      <th scope="col">Ссылка</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="event in filteredEvents">
+      <th scope="row">{{event.id}}</th>
+      <td>{{event.name}}</td>
+      <td>{{event.description}}</td>
+      <td><a :href="event.site">{{event.site}}</td>
+    </tr>
+  </tbody>
+</table>
+'
+                ]
             ],
             '#attached' => [
                 'library' => [
