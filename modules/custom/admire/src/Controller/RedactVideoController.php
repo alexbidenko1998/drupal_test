a337885a6f497bfad1546cdab41729a2df9b2b6f
Alexander Bidenko <alexbidenko1998@gmail.com>
2020-01-11 18:32:11 +0300

    Test

--- modules/custom/admire/src/Controller/RedactVideoController.php
+++ modules/custom/admire/src/Controller/RedactVideoController.php
@@ -7,12 +7,65 @@ use Drupal\Core\Controller\ControllerBase;
 class RedactVideoController extends ControllerBase {
 
     public function content($videoId) {
+        $form['title'] = array(
+            '#type' => 'textfield',
+            '#title' => 'Название',
+            '#name' => 'title',
+            '#attributes' => array('v-model' => 'title')
+        );
+        $form['description'] = array(
+            '#type' => 'textarea',
+            '#title' => 'Описание',
+            '#name' => 'description',
+            '#attributes' => array('v-model' => 'description')
+        );
+        $form['isPaid'] = array(
+            '#type' => 'checkbox',
+            '#title' => 'Является ли платным',
+            '#name' => 'isPaid',
+            '#attributes' => array('v-model' => 'isPaid')
+        );
+        $form['price'] = array(
+            '#type' => 'number',
+            '#title' => 'Цена',
+            '#name' => 'price',
+            '#attributes' => array(
+                'v-model' => 'price',
+                ':disabled' => 'isPaid'
+            )
+        );
+        $form['video'] = array(
+            '#type' => 'file',
+            '#title' => 'Загрузите видео',
+            '#name' => 'video',
+            '#attributes' => array(
+                '@change' => 'addVideo',
+                'id' => 'inputVideo'
+            )
+        );
+        $form['preview'] = array(
+            '#type' => 'file',
+            '#title' => 'Загрузите превью',
+            '#name' => 'preview',
+            '#attributes' => array(
+                '@change' => 'addPreview',
+                'id' => 'inputPreview'
+            )
+        );
+        $form['submit'] = array(
+            '#type' => 'button',
+            '#title' => 'Сохранить',
+            '#attributes' => array(
+                '@click' => 'submit',
+                ':disabled' => 'isDisabled'
+            )
+        );
         return [
             'title' => 'Редактировать видео',
             'body' => [
                 '#prefix' => '<div id="AppAdd" v-show="videoData" video-id="' . $videoId . '">',
                 '#sufix' => '</div>',
-                //'addForm' => $form,
+                'addForm' => $form,
                 'preview' => [
                     '#type' => 'markup',
                     '#markup' =>
